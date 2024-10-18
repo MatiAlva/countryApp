af561bf515376712fd3b5aaa7d0a49567363d6f6
MatiAlva <matyy01.ma@gmail.com>
2024-10-18 11:25:18 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from "./shared/shared.module";
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    SharedModule
+],
+  providers: [
+    provideHttpClient(),
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
